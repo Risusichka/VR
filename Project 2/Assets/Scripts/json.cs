b678fdaf6f4de68a3bb26c478d9fc2787810d7f1
Risusichka <68230436+Risusichka@users.noreply.github.com>
2024-12-23 20:53:24 +0300

    Pr 2 + Pr 5

---
+++ Project 2/Assets/Scripts/json.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;//для корректной работы интерфейса 
+using UnityEngine.Networking;//связанны с событиями, которые связанны с интернетом  
+using UnityEngine.EventSystems; //для обработки событий 
+
+public class json : MonoBehaviour
+{
+    public Text nm;
+    public Text lvl;
+    public Slider dat;
+    public Text dat1;//чтобы показать соответствие 
+    public string jsonURL;//ссылка на облако 
+    public Jsonclass jsnData;
+    // Start is called before the first frame update 
+    void Start()
+    {
+        dat.interactable = false;
+        StartCoroutine(getData());//выполняется автоматически, как только будет запущено 
+    }
+    IEnumerator getData()
+    {
+        Debug.Log("Загрузка...");
+        var uwr = new UnityWebRequest(jsonURL);//запрашиваем по ссылке данные 
+        uwr.method = UnityWebRequest.kHttpVerbGET;
+        var resultFile = Path.Combine(Application.persistentDataPath, "result.json");//запись в локальное хранилище 
+        var dh = new DownloadHandlerFile(resultFile);//запись данных из  массива в файл 
+        dh.removeFileOnAbort = true;//при ошибках загрузки 
+        uwr.downloadHandler = dh;//буфферизация 
+        yield return uwr.SendWebRequest();//точка, где выполнение будет приостановлено и возобновится в следующем кадре 
+
+        if (uwr.result != UnityWebRequest.Result.Success)
+        {
+            nm.text = "Ошибка!";
+        }
+        else
+        {
+            Debug.Log("Файл сохранен по пути: " + resultFile);
+            jsnData = JsonUtility.FromJson<Jsonclass>(File.ReadAllText(Application.persistentDataPath + "/result.json"));//отвечает за чтение из файла 
+            nm.text = jsnData.Name.ToString();
+            lvl.text = jsnData.Level.ToString();
+            dat.value = jsnData.TestParam;
+            dat1.text = jsnData.TestParam.ToString();
+            yield return StartCoroutine(getData());
+        }
+    }
+    [System.Serializable]
+    public class Jsonclass
+    {
+        public string Name;
+        public int Level;
+        public int TestParam;
+    }
+    // Update is called once per frame 
+    void Update()
+    {
+
+    }
+}
